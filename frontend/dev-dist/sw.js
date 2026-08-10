da8c13a539d3bcb771d8104f0061dd0ae73d3222
Durgesh <durgeshkanjariya10@gmail.com>
2026-08-10 12:42:47 +0530

    chore: update dev service worker (auto-generated by Vite)

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ps9nncq23h"
+    "revision": "0.jbv6dcqbd94"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
